5757f0eb10f9cbe02c75374376823447d28ae683
rene vannaken <rvannaken@sandiego.edu>
2024-03-30 17:36:07 -0400

    spring boot set up

---
+++ src/main/java/Puppygirl/WoofChatApplication.java
@@ -0,0 +1,13 @@
+package Puppygirl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoofChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoofChatApplication.class, args);
+	}
+
+}
